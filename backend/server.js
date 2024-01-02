5a3d3ce9a0bd239d2c75e3703161cbd4b57fdddb
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2024-01-03 03:10:00 +0600

    delivery person frontend auth working

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ mongoDB();
 
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     next();
+// })
 
 app.get('/', (req, res) => {
     res.send("working")
